91895fec2737f7a6121e68059dc06db64ddfd711
Poubilas <pobeschin@gmx.de>
2025-08-07 10:24:47 +0200

    Lautstärkemodul mit Einstellung sleep 4 in launch.sh

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,6 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Verzögerung einstellen, damit Volumemodul rechts erscheint
+sleep 4
+
 # for multimonitor
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
