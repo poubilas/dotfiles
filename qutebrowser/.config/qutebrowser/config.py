b5fb18b83a3e27a3da4c90f1c7d337b742beaa82
Poubilas <pobeschin@gmx.de>
2025-08-31 19:09:25 +0200

    In Qutebrowser geöffnete Tabs speichern und Global Zoomlever auf 130%

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,2 +1,11 @@
+# Lade automatisch gespeicherte Einstellungen.
 config.load_autoconfig()
+
+# Behalte die Sitzung beim Schließen immer bei.
+c.auto_save.session = True
+
+# Standardzoomlevel ändern
+c.zoom.default = '130%'
+
+
 
