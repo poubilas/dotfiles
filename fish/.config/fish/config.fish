e16d6b2ebdf0589ef17ff62e6728331780dfbaeb
Poubilas <pobeschin@gmx.de>
2025-09-09 18:28:17 +0200

    Mit lynx-config alles im dotfiles-Ordner

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,3 +5,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #function fish_greeting
 #    # smth smth
 #end
+
+# Benutzerdefinierte Konfiguration für den Webbrowser Lynx
+set -x LYNX_CFG "$HOME/.config/lynx/lynx.cfg"
+set -x LYNX_LSS "$HOME/.config/lynx/lynx.lss"
