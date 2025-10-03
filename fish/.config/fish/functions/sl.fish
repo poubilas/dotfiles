8910fcc68b20ff674e07d7f2aee8ab2b564d7dc5
poubilas <pobeschin@gmx.de>
2025-10-03 10:24:51 +0200

    Create sl.fish

---
+++ fish/.config/fish/functions/sl.fish
@@ -0,0 +1,26 @@
+function sl --description "Bildschirm aus; nach X Minuten herunterfahren"
+    set -l mins $argv[1]
+
+    if test -z "$mins"
+        echo "Nutzung: sl <Minuten>"
+        return 1
+    end
+
+    if not string match -rq '^[0-9]+$' -- $mins
+        echo "Fehler: <Minuten> muss eine ganze Zahl sein."
+        return 1
+    end
+
+    set -l secs (math "$mins * 60")
+    echo "Fahre in $mins Minute(n) herunter… (Ctrl+C zum Abbrechen)"
+
+    # Bildschirm ausschalten
+    if type -q kscreen-doctor
+        kscreen-doctor --dpms off
+    else if type -q xset
+        xset dpms force off
+    end
+
+    sleep $secs
+    systemctl poweroff
+end
