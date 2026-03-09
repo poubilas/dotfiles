fc4f3d5151b12547654fc1e4ef53e29f08a25860
poubilas <pobeschin@gmx.de>
2026-03-09 17:26:17 +0100

    Source local ls function in Fish config
    Add sourcing of local ls function overrides

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,3 +9,6 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # Benutzerdefinierte Konfiguration für den Webbrowser Lynx
 set -x LYNX_CFG "$HOME/.config/lynx/lynx.cfg"
 set -x LYNX_LSS "$HOME/.config/lynx/lynx.lss"
+
+# Re-apply local overrides after the CachyOS config defines its aliases.
+source ~/.config/fish/functions/ls.fish
